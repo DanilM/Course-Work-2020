0dfed04f85e6be0334b959939f4c9c5d41ad0f88
DanilM <warmi963652@gmail.com>
2021-04-24 15:18:24 +0300

    first commit

---
+++ Kyrsovaya/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+
+namespace Kyrsovaya
+{
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+
+    private string path1;
+    public byte[] pixels;
+    //public byte[] pixelsRight;
+    public BitmapImage Bmp1;
+
+    public byte[] Answer;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void First_Button(object sender, RoutedEventArgs e)
+    {
+      OpenFileDialog img = new OpenFileDialog();
+      
+      img.ShowDialog();
+      if (img.FileName != "")
+      {
+        path1 = img.FileName;
+        FirstPath.Text = path1;
+        Bmp1 = new BitmapImage(new Uri(img.FileName));
+        pixels = new byte[Bmp1.PixelWidth* Bmp1.PixelHeight*4];
+        Bmp1.CopyPixels(pixels, Bmp1.PixelWidth * 4, 0);
+        
+        //вывод картинки на экран
+        BitmapImage src = new BitmapImage();
+        src.BeginInit();
+        src.UriSource = new Uri(path1);
+        src.EndInit();
+        FirstArea.Source = src;
+      }
+      else
+      {
+        MessageBox.Show("Файл не выбран");
+      }
+    }
+
+    private void Save_Button(object sender, RoutedEventArgs e)
+    {
+      if (Answer != null)
+      {
+        SaveFileDialog dlg = new SaveFileDialog();
+        dlg.FileName = "Image";
+        dlg.DefaultExt = ".jpg";
+        dlg.Filter = "Text documents (.jpg)|*.jpg";
+        bool? result = dlg.ShowDialog();
+
+        if (result == true)
+        {
+          string filename = dlg.FileName;
+          SecondPath.Text = filename;
+
+          BitmapSource image1 = BitmapSource.Create(Bmp1.PixelWidth, Bmp1.PixelHeight, Bmp1.DpiX, Bmp1.DpiY, Bmp1.Format, Bmp1.Palette, Answer, Bmp1.PixelWidth * 4);
+
+          FileStream stream = new FileStream(filename, FileMode.Create);
+
+          SecondArea.Source = image1;
+          JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+          encoder.Frames.Add(BitmapFrame.Create(image1));
+          encoder.Save(stream);
+          stream.Close();
+        }
+      }
+      else
+      {
+        MessageBox.Show("Вы не запустили программу");
+      }
+    }
+
+    private void Start_Button(object sender, RoutedEventArgs e)
+    {
+      if (path1 != null)
+      {
+        Answer = new byte[Bmp1.PixelWidth * Bmp1.PixelHeight * 4];
+        for (int i = 0; i < pixels.Length; i += 4)
+        {
+          Perceptron core = new Perceptron(Convert.ToDouble(pixels[i]) / 255, Convert.ToDouble(pixels[i + 1]) / 255, Convert.ToDouble(pixels[i + 2]) / 255);
+          ref double[] r = ref core.Start();
+          for (int j = 0; j < 3; j++)
+          {
+            {
+              Answer[i + j] = Convert.ToByte(Math.Round(r[j] * 255));
+            }
+            Answer[i + 3] = 255;
+          }
+        }
+
+        BitmapSource image1 = BitmapSource.Create(Bmp1.PixelWidth, Bmp1.PixelHeight, Bmp1.DpiX, Bmp1.DpiY, Bmp1.Format, Bmp1.Palette, Answer, Bmp1.PixelWidth * 4);
+        SecondArea.Source = image1;
+      }
+      else
+      {
+        MessageBox.Show("Вы не выбрали путь к файлу");
+      }
+    }
+   }
+}
+  
---
+++ Kyrsovaya/Perceptron.cs
@@ -0,0 +1,151 @@
+﻿using Kyrsovaya.Properties;
+using System;
+using System.IO;
+
+namespace Kyrsovaya
+{
+  class Perceptron
+  {
+    public Perceptron(double InputValue1, double InputValue2, double InputValue3)
+    {
+      this.InputValue[0] = InputValue1;
+      this.InputValue[1] = InputValue2;
+      this.InputValue[2] = InputValue3;
+      //this.OutputRight[0] = OutputRight1;
+      //this.OutputRight[1] = OutputRight2;
+      //this.OutputRight[2] = OutputRight3;
+    }
+
+    public string path1 { get; set; }
+    public string path2 { get; set; }
+    public string path3 { get; set; }
+
+    public double[,] InputWeight = new double[4, 3];
+    public double[,] HiddenWeight = new double[3, 4];
+
+    public double[] InputValue = new double[3];
+    public double[] SecondValue = new double[4];
+    public double[] OutputReal = new double[3];
+    public double[] OutputRight = new double[3];
+
+    public double[] DeltaHidden = new double[4];
+
+    public void Calc()
+    {
+      double res;
+      for (int i = 0; i < InputWeight.GetLength(0); i++)
+      {
+        res = 0;
+        for (int j = 0; j < InputWeight.GetLength(1); j++)
+        {
+          res += InputWeight[i, j] * InputValue[j];
+        }
+        SecondValue[i] = Sigmoid(res);
+      }
+
+      for (int i = 0; i < HiddenWeight.GetLength(0); i++)
+      {
+        res = 0;
+        for (int j = 0; j < HiddenWeight.GetLength(1); j++)
+        {
+          res += HiddenWeight[i, j] * SecondValue[j];
+        }
+        OutputReal[i] = Sigmoid(res);
+      }
+    }
+
+    public double Sigmoid(double x)
+    {
+      return 1 / (1 + Math.Exp((-1) * x));
+    }
+
+    
+    public ref double[] Start()
+    {
+      
+      GetWeight();
+      Calc();
+      //FindDelta();
+      //Correction();
+      //SaveWeight();
+      return ref OutputReal;
+    }
+
+    public void FindDelta()
+    {
+      double res;
+      for (int j = 0; j < HiddenWeight.GetLength(1); j++)
+      {
+        res = 0;
+        for (int i = 0; i < HiddenWeight.GetLength(0); i++)
+        {
+          res += HiddenWeight[i, j] * (OutputRight[i] - OutputReal[i]);
+        }
+        DeltaHidden[j] = res;
+        
+      }
+    }
+
+    public void Correction()
+    {
+      for (int i = 0; i < InputWeight.GetLength(0); i++)
+      {
+        for (int j = 0; j < InputWeight.GetLength(1); j++)
+        {
+          InputWeight[i, j] += DeltaHidden[i] * SecondValue[i] * (1 - SecondValue[i]) * InputValue[j] * 0.5;
+        }
+      }
+
+      for (int j = 0; j < HiddenWeight.GetLength(1); j++)
+      {
+        for (int i = 0; i < HiddenWeight.GetLength(0); i++)
+        {
+          HiddenWeight[i, j] += (OutputRight[i] - OutputReal[i]) * OutputReal[i] *(1 - OutputReal[i]) * SecondValue[j] * 0.5;
+        }
+      }
+    }
+
+    public void GetWeight()
+    {
+      string pathWeight = @"Весы.txt";
+      StreamReader RS = new StreamReader(pathWeight);
+      for (int i = 0; i < InputWeight.GetLength(0); i++)
+      {
+        for (int j = 0; j < InputWeight.GetLength(1); j++)
+        {
+          InputWeight[i, j] = double.Parse(RS.ReadLine());
+        }
+      }
+      for (int i = 0; i < HiddenWeight.GetLength(0); i++)
+      {
+        for (int j = 0; j < HiddenWeight.GetLength(1); j++)
+        {
+          HiddenWeight[i, j] = double.Parse(RS.ReadLine());
+        }
+      }
+      RS.Close();
+    }
+
+    public void SaveWeight()
+    {
+      string pathWeight = @"Весы.txt";
+      StreamWriter WS = new StreamWriter(pathWeight);
+      for (int i = 0; i < InputWeight.GetLength(0); i++)
+      {
+        for (int j = 0; j < InputWeight.GetLength(1); j++)
+        {
+          //Console.WriteLine(InputWeight[i, j]);
+          WS.WriteLine(InputWeight[i, j]);
+        }
+      }
+      for (int i = 0; i < HiddenWeight.GetLength(0); i++)
+      {
+        for (int j = 0; j < HiddenWeight.GetLength(1); j++)
+        {
+          WS.WriteLine(HiddenWeight[i, j]);
+        }
+      }
+      WS.Close();
+    }
+  }
+}
